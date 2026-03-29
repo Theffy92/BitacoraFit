3114197c6c9b944d8e6a4cd1b724a1ad8e2e4b10
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2026-03-29 19:06:05 -0400

    Add first model - WeightEntry

--- src/tracking/models.py
+++ src/tracking/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class WeightEntry(models.Model):
+    #defining the fields for the weight entry model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    #Storing weight in pounds with up to 2 decimal places
+    #idea: Convert it to kilograms in the view (UI) if needed, but store it in pounds for consistency
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date}: {self.weight} lbs"
