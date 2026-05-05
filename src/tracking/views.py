964cadaaabbd04bd43bd7205f1151e19f4fb476c
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2026-05-05 18:20:07 -0400

    Create weight_dashboard file,add weight_dashboard function in views file

--- src/tracking/views.py
+++ src/tracking/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import WeightEntry
 # Create your views here.
+def weight_dashboard(request):
+    # Fetching all weight entries
+    weight_entries = WeightEntry.objects.filter(user=request.user).order_by('-date', "created_at")
+    latest_entry = weight_entries.first()  # Get the most recent entry
+    recent_entries = weight_entries[1:6]  # Get the next 5 recent entries after the latest one
+    return render(request, 'tracking/weight_dashboard.html', {'latest_entry': latest_entry, 'recent_entries': recent_entries})
