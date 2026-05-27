5b591e38573c6d6d9d2823281aea39020b3e736a
Elena Estefania Reyes <estefyreyes.92@gmail.com>
2026-05-26 20:11:50 -0400

    Addlogin page, WIP

--- src/config/settings.py
+++ src/config/settings.py
@@ -142,4 +142,5 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+LOGIN_REDIRECT_URL = 'weight_dashboard'
+LOGOUT_REDIRECT_URL = 'login'
--- src/config/urls.py
+++ src/config/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('tracking.urls')),
 ]
 
